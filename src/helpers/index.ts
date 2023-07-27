a55dcf405dbc0ade063b9a40bb99cab9c71871a5
mgregerson <mgregerson4321@gmail.com>
2023-07-27 17:58:50 -0400

    Added controller for authentication:
    1. register: authenticates and creates new user. Returns userData on success.
    2. login: authenticates existing user vs the database. Returns userData on success.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,11 @@
 import crypto from "crypto";
 
+const SECRET = "MG-REST-API";
+
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string, password: string) => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))
-    .update(process.env.SECRET)
+    .update(SECRET)
     .digest("hex");
 };
