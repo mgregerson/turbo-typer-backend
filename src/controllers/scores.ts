694b324757c31716e2d3773b1c739f443430b485
mgregerson <mgregerson4321@gmail.com>
2023-08-16 10:59:52 -0400

    Added controllers for scores Model. All correspond to db functions

---
+++ src/controllers/scores.ts
@@ -0,0 +1,95 @@
+import express from "express";
+import {
+  getScoreById,
+  getScoresByUser,
+  getScoresByTypingTest,
+  getScoresByUserAndTypingTest,
+  getScoresByUserAndTypingTestAndDate,
+  createNewScore,
+} from "../db/scores";
+
+export const retrieveScoreById = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const score = await getScoreById(req.params.id);
+    return res.status(200).json(score);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const retrieveScoresByUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const scores = await getScoresByUser(req.params.id);
+    return res.status(200).json(scores);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const retrieveScoresByTypingTest = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const scores = await getScoresByTypingTest(req.params.id);
+    return res.status(200).json(scores);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const retrieveScoresByUserAndTypingTest = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const scores = await getScoresByUserAndTypingTest(
+      req.params.userId,
+      req.params.typingTestId
+    );
+    return res.status(200).json(scores);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const retrieveScoresByUserAndTypingTestAndDate = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const currentDate = new Date();
+    const scores = await getScoresByUserAndTypingTestAndDate(
+      req.params.userId,
+      req.params.typingTestId,
+      currentDate
+    );
+    return res.status(200).json(scores);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const createScore = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const score = await createNewScore(req.body);
+    return res.status(200).json(score);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
